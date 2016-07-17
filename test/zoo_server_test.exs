c381728dac2447d2c8a14c5886ab8254a667d993
Jingjing Duan <jduan@fitbit.com>
2016-07-16 21:41:40 -0700

    add unit tests

--- test/zoo_server_test.exs
+++ test/zoo_server_test.exs
@@ -1,8 +1,29 @@
 defmodule ZooServerTest do
   use ExUnit.Case
   doctest ZooServer
+  alias ZooServer.Models
+  alias ZooServer.Client
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @animal Models.Animal.new(
+    type: Models.AnimalType.elephant,
+    name: "daizy",
+    weight: 100
+  )
+
+  setup do
+    response = create_animal()
+    on_exit fn ->
+      :ets
+    end
+  end
+
+  test "create and get animal" do
+    response = create_animal()
+    assert %{@animal | id: "0"} == response.animal
+  end
+
+  defp create_animal() do
+    request = Models.AnimalsCreateRequest.new(animal: @animal)
+    Client.create(request)
   end
 end
