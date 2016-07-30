2a2ba008f783cda86e6120ea4721cb747782c038
Jingjing Duan <jduan@fitbit.com>
2016-07-29 22:28:41 -0700

    bring back manual_test.exs

--- test/manual_test.exs
+++ test/manual_test.exs
@@ -1,15 +1,12 @@
 # create an animal
-# animal = ZooServer.Models.Animal.new(
-#   id: "1",
-#   type: ZooServer.Models.AnimalType.elephant,
-#   name: "daizy")
-# request = ZooServer.Models.AnimalsCreateRequest.new(animal: animal)
-# ZooServer.Client.create(request)
-#
-# # get an animal that exists
-# request = ZooServer.Models.AnimalsGetRequest.new(id: "0")
-# ZooServer.Client.get(request)
-#
-# # get an animal that doesn't exist
-# request = ZooServer.Models.AnimalsGetRequest.new(id: "1")
-# ZooServer.Client.get(request)
+animal = ZooServer.Helper.animal("pluto", 50, :monkey)
+request = ZooServer.Models.AnimalsCreateRequest.new(animal: animal)
+ZooServer.Client.create(request)
+
+# get an animal that exists
+request = ZooServer.Models.AnimalsGetRequest.new(id: "0")
+ZooServer.Client.get(request)
+
+# get an animal that doesn't exist
+request = ZooServer.Models.AnimalsGetRequest.new(id: "1")
+ZooServer.Client.get(request)
